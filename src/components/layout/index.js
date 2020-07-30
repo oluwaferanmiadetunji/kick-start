ce7092a1604de762c9d781e7b2bdbb3ac367be1a
Adetunji Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2020-07-30 21:32:29 +0100

    .

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
 import './style.css';
 
 const Layout = (Component) => () => {
 	return (
-		<Grid className='layout'>
-			<div>
-				<Typography variant='h4'>Kick Start</Typography>
-			</div>
-			<Component />
-		</Grid>
+		<div className='layout'>
+			<Grid className='header'>
+				<Grid item md={6} lg={6} id='logo'>
+					<Typography variant='h4'>
+						<Link to='/'>Kick Start</Link>
+					</Typography>
+				</Grid>
+				<Grid item md={6} lg={6} className='navigation__links'>
+					<Link to='/login'>Login</Link>
+				</Grid>
+			</Grid>
+			<Grid className='component'>
+				<Component />
+			</Grid>
+		</div>
 	);
 };
 
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import { components as LoginPage } from './components/login';
+import { components as LandingPage } from './components/landingPage';
 
 export const Routes = () => (
 	<Router>
 		<Switch>
 			<Route exact path='/login' component={LoginPage} />
+			<Route exact path='/' component={LandingPage} />
 		</Switch>
 	</Router>
 );
