2c57dfd86b6cc802a5f435481c7f18e35f7d7f16
Adetunji Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2020-07-31 16:23:33 +0100

    .

---
+++ src/components/firebaseConfig/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyD1Tn8yDWqFfrk5bdUhO0becBAvpspQtwg',
+	authDomain: 'kick-start-ba74b.firebaseapp.com',
+	databaseURL: 'https://kick-start-ba74b.firebaseio.com',
+	projectId: 'kick-start-ba74b',
+	storageBucket: 'kick-start-ba74b.appspot.com',
+	messagingSenderId: '664030078380',
+	appId: '1:664030078380:web:008258fe31dd88d0c0c609',
+	measurementId: 'G-4V7N6JN5FH',
+};
+
+export default firebaseConfig;
