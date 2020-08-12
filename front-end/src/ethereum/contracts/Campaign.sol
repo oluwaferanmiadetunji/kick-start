47d42ae011dc8ab9e5368e5e211bac6f6913198a
Adetunji Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2020-08-12 20:14:47 +0100

    .

--- front-end/src/ethereum/contracts/Campaign.sol
+++ front-end/src/ethereum/contracts/Campaign.sol
@@ -1,36 +1,95 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.7.0;
+pragma solidity ^0.4.17;
 
 contract CampaignFactory {
-    Campaign[] public deployedCampaigns;
-
+    address[] public deployedCampaigns;
+    
     function createCampaign(uint minimum) public {
-        Campaign newCampaign = new Campaign(minimum, msg.sender);
+        address newCampaign = new Campaign(minimum,msg.sender);
         deployedCampaigns.push(newCampaign);
-    } 
-
-    function getDeployedCampaigns() public view returns (Campaign[] memory) {
+    }
+    
+    function getDeployedCampaigns () public view returns (address[]) {
         return deployedCampaigns;
     }
 }
 
 contract Campaign {
-    constructor (uint minimum, address creator) {
-        address manager = creator;
-        uint minimumContribution = minimum;
-    }
-
-    struct NewRequest {
-        uint amount;
-        uint approvalCount;
+    struct Request {
         string description;
+        uint amount;
         address recipient;
         bool complete;
+        uint approvalCount;
         mapping(address => bool) approvals;
     }
-
-    address public manager;
+    
+    Request[] public requests;
+    
+    address public manager; 
     uint public minimumContribution;
     mapping(address => bool) public approvers;
-    uint public numberOfApprovers;
+    uint public approversCount;
+ 
+    modifier restricted () {
+        require(msg.sender == manager);
+        _;
+    }
+
+    function Campaign (uint minimum, address creator) public {
+        manager = creator;
+        minimumContribution = minimum;
+    }
+ 
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+        approvers[msg.sender] = true;
+        approversCount++;
+    }
+    
+    function createRequest (string description, uint amount, address recipient) public restricted {
+        Request memory newRequest = Request({
+           description: description, 
+           amount:amount, 
+           recipient:recipient, 
+           complete:false, 
+           approvalCount:0
+        });
+        
+        requests.push(newRequest);
+    }
+    
+    function approveRequest (uint index) public {
+        Request storage request = requests[index];
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+    
+    function finalizeRequest (uint index) public restricted {
+        Request storage request = requests[index];
+        require(!request.complete);
+        require(request.approvalCount > (approversCount / 2));
+        request.recipient.transfer(request.amount);
+        
+        request.complete = true;
+        
+    }
+
+    function getSummary () public view returns (
+        uint,uint,uint,uint,address
+    ) {
+        return (
+            minimumContribution,
+            this.balance,
+            requests.length,
+            approversCount,
+            manager
+        );
+    }
+
+    function getRequestsCount () public view returns (uint) {
+        return requests.length;
+    }
 }
