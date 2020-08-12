69f2b7b7c989060f7059ab28885783c76109c94f
Adetunji Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2020-08-12 23:05:16 +0100

    .

--- front-end/src/components/landingPage/component/LandingPage.js
+++ front-end/src/components/landingPage/component/LandingPage.js
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 
-function page() {
+import factory from '../../../web3/factory';
+
+const LandingPage = () => {
+  const [campaigns, setCampaigns] = useState([]);
+
+	useEffect(() => {
+		async function getAllCampaigns() {
+      const AllCampaigns = await factory.methods.getDeployedCampaigns().call();
+      console.log(AllCampaigns);
+			setCampaigns(AllCampaigns);
+		}
+		getAllCampaigns();
+	}, []);
+
 	return (
-		<Grid container spacing={2}>
-			<Grid item xs={12} sm={12} md={4} lg={4} className='component'>
-				<Card>
+		<Grid container id='container' spacing={4}>
+			<Grid item xs={12} sm={12} md={6} lg={6} className='component'>
+				<Card id='component__card'>
 					<CardContent>
-            
-          </CardContent>
+						<div style={{textAlign: 'left'}}>
+							<Typography variant='body1' style={{marginBottom: 10}}>
+								Manager: Adetunji Oluwaferanmi
+							</Typography>
+							<Typography variant='body1' style={{marginBottom: 10}}>
+								Total: 100 Ether
+							</Typography>
+							<Typography variant='body1'>Balance: 100 Ether</Typography>
+						</div>
+						<Button style={{background: 'red', color: 'white', marginTop: 10, textAlign: 'center'}} size='small'>
+							<Link to='/'>
+								<KeyboardArrowRightIcon />
+							</Link>
+						</Button>
+					</CardContent>
 				</Card>
 			</Grid>
 		</Grid>
 	);
-}
+};
 
-export default page;
+export default LandingPage;
